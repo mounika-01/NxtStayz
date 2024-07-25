df6919c7efacd307a65df31f9796faf0780a54d3
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-07-25 13:12:45 +0530

    Update HotelJpaService.java

--- src/main/java/com/example/nxtstayz/service/HotelJpaService.java
+++ src/main/java/com/example/nxtstayz/service/HotelJpaService.java
@@ -69,4 +69,14 @@ public class HotelJpaService implements HotelRepository {
         }
         throw new ResponseStatusException(HttpStatus.NO_CONTENT);
     }
+
+    @Override
+    public List<Room> getHotelRooms(int hotelId) {
+        try {
+            Hotel hotel = hotelJpaRepository.findById(hotelId).get();
+            return roomJpaRepository.findByHotel(hotel);
+        }catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
 }
